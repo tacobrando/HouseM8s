fbf1dee7a4701a15f5cb94d9f5cc922c1fa2820f
tacobrando <matthewytlee2@gmail.com>
2024-01-13 00:23:40 +0000

    samesite lax

--- server/lib/Cookie.js
+++ server/lib/Cookie.js
@@ -2,7 +2,9 @@ export const Cookie = Object.freeze({
   options: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    sameSite: 'strict',
+    path: '/',
+    domain: 'housem8s-2082efe081e6.herokuapp.com',
     maxAge: 604800000,
   }
 })
