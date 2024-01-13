0b2cabaf4dab60f8cd1a46fd18a07d51d1e78ed8
tacobrando <matthewytlee2@gmail.com>
2024-01-13 01:58:36 +0000

    login

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <AppLayout>
     <router-view />
-    <button v-if="showInstallPrompt" @click="promptInstall">
-      Install App
-    </button>
   </AppLayout>
 </template>
 
 <script setup>
-import { onBeforeMount, ref } from 'vue';
+import { onBeforeMount } from 'vue';
 import { useChoreStore } from '@/store/chore';
 import { useGroceryStore } from '@/store/grocery';
 import { useSettings } from '@/composables/settings';
@@ -22,31 +19,10 @@ const socket = useSocket()
 const groceryStore = useGroceryStore()
 const choreStore = useChoreStore()
 
-const showInstallPrompt = ref(false);
-let deferredPrompt;
-
 onBeforeMount(() => {
   socket.init()
   settings.init();
   groceryStore.init(SocketEvent.GROCERY)
   choreStore.init(SocketEvent.CHORE)
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-    showInstallPrompt.value = true;
-  });
 })
-
-async function promptInstall() {
-  if (!deferredPrompt) return;
-
-  deferredPrompt.prompt();
-  const { outcome } = await deferredPrompt.userChoice;
-
-  console.log(`User response to the install prompt: ${outcome}`);
-  
-  deferredPrompt = null;
-  showInstallPrompt.value = false;
-};
 </script>
