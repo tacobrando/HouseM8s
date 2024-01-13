5246e6d72c67b9879e1807ee959bc7b04b4bdb54
tacobrando <matthewytlee2@gmail.com>
2024-01-13 00:44:54 +0000

    cookie options

--- server/lib/Session.js
+++ server/lib/Session.js
@@ -6,6 +6,7 @@ export const Session = Object.freeze({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
+    proxy : true,
     store: MongoStore.create({
       mongoUrl: process.env.MONGO_URI
     }),
