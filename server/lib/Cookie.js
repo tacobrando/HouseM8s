4b428dfc77b9c6f05eb0530a4147ed0f7b762ec8
tacobrando <matthewytlee2@gmail.com>
2024-01-13 00:34:05 +0000

    cookie options

--- server/lib/Cookie.js
+++ server/lib/Cookie.js
@@ -1,10 +1,9 @@
 export const Cookie = Object.freeze({
   options: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'None',
     path: '/',
-    domain: 'housem8s-2082efe081e6.herokuapp.com',
     maxAge: 604800000,
   }
 })
